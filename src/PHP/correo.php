f25c28d3280983c329468ceb604328f86cdf4ac2
Charlescorte98 <92405870+Charlescorte98@users.noreply.github.com>
2022-02-08 10:32:29 -0600

    Add files via upload

---
+++ src/PHP/correo.php
@@ -0,0 +1,25 @@
+<?php   
+
+    $destino = "atlantis.tecno.advanced@gmail.com";
+
+if (isset($_POST['enviar'])) {
+    if (!empty($_POST['name']) && ($_POST['email']) && ($_POST['celular']) && ($_POST['msg'])) {
+        $name = ($_POST['name']);
+        $email = ($_POST['email']);
+        $celular = ($_POST['celular']);
+        $msg = ($_POST['msg']);
+
+        $header = "From: noreply@adddmex.com" . "\r\n";
+        $header .= "Reply-To: noreply@adddmex.com" . "\r\n";
+        $header .= "X-Mailer: PHP/". phpversion();
+
+        $mail = mail($destino,$email,$celular,$msg,$header);
+        if ($mail) {
+            echo "<h4>!Mail Enviado Exitosamente¡</h4>";
+            header('Location: index.html');
+        }
+        header('Location: ../../index.html');
+    }
+}
+
+?>
